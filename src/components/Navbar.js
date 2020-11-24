9a385fc4e1e446ec39d46b2dcd68d47994a7a4f2
Fritz-Calimag <Fritz.Calimag@gmail.com>
2020-11-24 18:14:20 +0800

    Changed the logo template to Aqwire logo, Changed the contents of the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Aqwire" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -60,20 +60,23 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/personal">
+                Personal
+              </Link>
+              <Link className="navbar-item" to="/enterprise">
+                enterprise
               </Link>
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+              <Link className="navbar-item" to="/about-us">
+                About Us
+              </Link>
+              <Link className="navbar-item" to="/help">
+                Help
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Contact Us
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
